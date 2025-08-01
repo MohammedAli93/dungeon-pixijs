07e86af655d160bbf913457e09d8c77f4822af7d
Andres <costandres8@gmail.com>
2025-07-31 22:24:02 -0400

    reset

--- src/lib/rex-plugins/plugins/awaitcomlinkloader.js
+++
@@ -1,5 +0,0 @@
-import LoaderCallback from './loader/awaitcomlink/AwaitComlinkCallback.js';
-
-Phaser.Loader.FileTypesManager.register('rexAwaitComlink', LoaderCallback);
-
-export default LoaderCallback;
